bd665ec686c0f2e12d18f6e6c0d4694a2dd5af11
viradm334 <viradamayanti@student.telkomuniversity.ac.id>
2025-06-28 11:44:06 +0700

    add src/index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
-  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      title: 'Todo App'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  mode: 'production'
 };
